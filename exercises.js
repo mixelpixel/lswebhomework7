80ba22a15c79bbf5899d5f69e7848211cb2d9aa4
mixelpixel <mixelpix@mac.com>
2017-06-10 08:33:54 -0400

    contains

--- exercises.js
+++ exercises.js
@@ -150,6 +150,9 @@ function wordsToSentence(words) {
 function contains(arr, item) {
   //check to see if item is inside of arr
   //return true if it is, otherwise return false
+  if (arr[item] === true) {
+    return true;
+  } else { return false; }
 }
 
 function addNumbers(numbers) {
