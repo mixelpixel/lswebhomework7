a9432562c6e392d26e408e4ca439942b7b51e959
mixelpixel <mixelpix@mac.com>
2017-06-10 08:50:43 -0400

    averageTestScores

--- exercises.js
+++ exercises.js
@@ -173,12 +173,18 @@ function addNumbers(numbers) {
 function averageTestScore(testScores) {
   //testScores is an array.  Iterate over testScores and compute the average.
   //return the average
-  var sum = 0;
-  var numTests = testScores.length;
-  for (var i = 0; i < testScores; i++) {
-    sum += testScores[i];
-  }
-  return sum / numTests;
+  // var sum = 0;
+  // var numTests = testScores.length;
+  // for (var i = 0; i < testScores; i++) {
+  //   sum += testScores[i];
+  // }
+  // return sum / numTests;
+  var total = 0;
+  var average = testScores.length;
+  for (var i = 0; i < testScores.length; i++) {
+    total = total + testScores[i];
+  } return total / average;
+
 }
 
 function largestNumber(numbers) {
